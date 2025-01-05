a97bef28041e85dc3fac14e20621708afdc37cdb
akash261999 <akashdongre433@gmail.com>
2025-01-05 06:42:35 +0530

    done

--- components/Helper/Home/NavBar/Nav.tsx
+++ components/Helper/Home/NavBar/Nav.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import Image from 'next/image';
+
 import { navlinks } from '@/constants/constant';
 import Link from 'next/link';
 import { HiBars3BottomRight } from 'react-icons/hi2';
